dfe70a6874eaa170482414b44553486774e0a0a6
Emanuel Santos Souza de Jesus <emanuelsantoss42@gmail.com>
2023-05-08 20:41:03 -0300

    a

--- Code/frontend/AccountOwnerClient/src/app/owner/owner.module.ts
+++ Code/frontend/AccountOwnerClient/src/app/owner/owner.module.ts
@@ -4,16 +4,25 @@ import { CommonModule } from '@angular/common';
 import { OwnerRoutingModule } from './owner-routing.module';
 import { OwnerListComponent } from './owner-list/owner-list.component';
 import { OwnerDetailsComponent } from './owner-details/owner-details.component';
+import { OwnerCreateComponent } from './owner-create/owner-create.component';
+
+
+import { ReactiveFormsModule } from '@angular/forms';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 
 
 @NgModule({
   declarations: [
     OwnerListComponent,
-    OwnerDetailsComponent
+    OwnerDetailsComponent,
+    OwnerCreateComponent
   ],
   imports: [
     CommonModule,
-    OwnerRoutingModule
+    OwnerRoutingModule,
+    ReactiveFormsModule,
+    BsDatepickerModule.forRoot()
+
   ]
 })
 export class OwnerModule { }
